36970478985f12c8889848e0ba1bd527cd20b063
张斯磊 <1055191226@qq.com>
2017-10-19 19:16:14 +0800

    添加热更新配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+/**
+ * @author https://github.com/zhangsilei 
+ * @description 生产环境配置文件
+ */
+
 var webpack = require('webpack');
 
 module.exports = {
     entry: './src/entry.js',
+    
     output: {
         path: __dirname + '/dist',
         filename: 'tool.min.js'
     },
+
+    devtool: 'eval-source-map', // 配置debug模式
+
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
-            minimize: true
+            compress: { // 压缩配置
+                warnings: false
+            },
+            mangle: true, // 开启混淆
+            minimize: true, // 开启压缩loaders
+            minify: true, // 开启最小化
+            sourcemap: true, // 生成soucemap文件，便于调试
         })
     ]
 }
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,23 @@
-var webpack = require('webpack');
+/**
+ * @author https://github.com/zhangsilei 
+ * @description 开发环境配置文件
+ */
 
 module.exports = {
     entry: './src/entry.js',
+
     output: {
-        path: __dirname + '/dist',
-        filename: 'tool.js'
+        filename: 'tool.js',
+        publicPath: '/asset/' // 设置虚拟目录
+    },
+
+    devServer: { 
+        historyApiFallback: true, // 页面不跳转，适用于spa应用
+        inline: true, // 开启自动刷新
+        port: 8080, // 默认使用8080端口
+        hot: true, // 开启模块热更新
+        compress: true, // 开启gzip压缩
+        progress: true, //显示打包进度
+        openPage: '/test/test.html', // 指定默认打开文件
     }
 }
